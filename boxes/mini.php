5e7ad59de39868b9b1dc2064dd1c85df5131a379
Esben Tind <esben@inkprodk.onmicrosoft.com>
2018-07-24 09:41:20 +0200

    Added link to mini trustbox

--- boxes/mini.php
+++ boxes/mini.php
@@ -16,14 +16,16 @@ $background_color = isset($_GET['bg_hex']) ? $_GET['bg_hex'] : "fff";
         <meta charset="utf-8">
     </head>
     <body style="background-color:#<?= $background_color; ?>">
-    <div id="content-wrapper">
-        <h3><img src="../static/img/trustpilot_logo_10x10.gif">Trustscore <?= $data->trust_score; ?></h3>
-        <div id="trust-stars">
-            <?php for($i=1; $i<=$data->stars; $i++): ?>
-            <span class="trust-star star-<?= $i; ?>">&#9733;</span>
-            <?php endfor; ?>
+    <a target="_blank" href="<?= $data->profile_url; ?>">
+        <div id="content-wrapper">
+            <h3><img src="../static/img/trustpilot_logo_10x10.gif">Trustscore <?= $data->trust_score; ?></h3>
+            <div id="trust-stars">
+                <?php for($i=1; $i<=$data->stars; $i++): ?>
+                <span class="trust-star star-<?= $i; ?>">&#9733;</span>
+                <?php endfor; ?>
+            </div>
+            <p id="review-count"><?= sprintf("%s anmeldelser",number_format($data->total_number_of_reviews,0,",",".")); ?></p>
         </div>
-        <p id="review-count"><?= sprintf("%s anmeldelser",number_format($data->total_number_of_reviews,0,",",".")); ?></p>
-    </div>
+    </a>
     </body>
 </html>
